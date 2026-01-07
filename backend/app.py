860f73aabcfe1947aa4328cf3158f7afd80a5ff4
Fazalul-Ameen <fazalulameen777@gmail.com>
2026-01-07 14:51:58 +0530

    Initial Commit

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+import flask
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
