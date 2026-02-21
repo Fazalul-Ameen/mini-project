1560ef5b2f44080ef71b016b513604af98575b45
Fazalul Ameen <fazalulameen777@gmail.com>
2026-02-21 14:01:27 +0530

    Pdf problem solved(2)

--- frontend/script.js
+++ frontend/script.js
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
         resultBox.innerHTML = "Processing...";
         resultBox.style.display = "block";
 
-        fetch("http://127.0.0.1/5000/predict", {
+        fetch("http://127.0.0.1:5000/predict", {
             method: "POST",
             body: formData
         })
